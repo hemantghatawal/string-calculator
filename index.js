a5a56186e28a48fde7eb9a6820e45c051016f549
Hemant Ghatawal <ghatawal35@gmail.com>
2025-04-12 09:47:43 +0530

    test case: handles an unknown amount of numbers

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ function Add(string) {
 
   const strChunk = string.split(",");
   strChunk.forEach((element) => {
+    if(element)
     sum += Number(element);
   });
 
   return sum;
 }
 
-console.log(Add(""));
+console.log(Add("1,2,3,4,5,6,7,8,9,10"));
 
 module.exports = { Add };
--- index.test.js
+++ index.test.js
@@ -7,3 +7,8 @@ test("if empty string return 0", () => {
 test("return number for single input", () => {
   expect(Add("1")).toBe(1);
 });
+
+test("handles an unknown amount of numbers", () => {
+  expect(Add("1,2,3,4,5")).toBe(15);
+  expect(Add("1,2,3,4,5,6,7,8,9,10")).toBe(55);
+});
